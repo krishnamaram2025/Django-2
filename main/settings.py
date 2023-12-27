35accacd5bb8ed96eb0f3b2218fff721c5d1d79d
krishnamaram2025 <112494492+krishnamaram2025@users.noreply.github.com>
2023-12-27 15:26:04 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ca)-d=o4tbnjscfsst62nb9-*)xt229gqv!yy7(t(cs@)pn8k=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
